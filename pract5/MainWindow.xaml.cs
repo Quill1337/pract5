55d49ebbd3920bcbcbd59b64d01ca103630c5a33
исп-31 <-31@LAB44-10>
2022-11-08 15:13:27 +0300

    Добавьте файлы проекта.

---
+++ pract5/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pract5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Information_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Митрофанов Роман Валерьевич ИСП-31\nСоздать класс Triad (тройка нечетных чисел).\n Создать необходимые методы и свойства.Определить метод сравнения двух триад на равенство.\nСоздать перегруженный метод сравнения трех триад на равенство.");
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void CompareNumber_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Triad triad = new Triad(int.Parse(firstTriadA.Text), int.Parse(secondTriadA.Text), int.Parse(thirdTriadA.Text));
+                Triad triad1 = new Triad(int.Parse(firstTriadB.Text), int.Parse(secondTriadB.Text), int.Parse(thirdTriadB.Text));
+
+                equal.Text = triad.CompareTwo(triad, triad1).ToString();
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void CompareNumber2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Triad triad = new Triad(int.Parse(firstTriadA.Text), int.Parse(secondTriadA.Text), int.Parse(thirdTriadA.Text));
+                Triad triad1 = new Triad(int.Parse(firstTriadB.Text), int.Parse(secondTriadB.Text), int.Parse(thirdTriadB.Text));
+                Triad triad2 = new Triad(int.Parse(firstTriadC.Text), int.Parse(secondTriadC.Text), int.Parse(thirdTriadC.Text));
+
+                equal.Text = triad.CompareThree(triad, triad1, triad2).ToString();
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ pract5/Triad.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract5
+{
+    internal class Triad
+    {
+        private int _first;
+        private int _second;
+        private int _third;
+
+        public Triad(int first, int second, int third)
+        {
+            _first = first;
+            _second = second;
+            _third = third;
+        }
+        public int First
+        {
+            get { return _first; }
+            set
+            {
+                if (value % 2 == 0)
+                {
+                    throw new ArgumentException($"{nameof(First)} Число должно быть нечетным");
+                }
+                _first = value;
+            }
+        }
+        public int Second
+        {
+            get { return _second; }
+            set
+            {
+                if (value % 2 == 0)
+                {
+                    throw new ArgumentException($"{nameof(Second)} Число должно быть нечетным");
+                }
+                _second = value;
+            }
+        }
+        public int Third
+        {
+            get { return _third; }
+            set
+            {
+                if (value % 2 == 0)
+                {
+                    throw new ArgumentException($"{nameof(Third)} Число должно быть нечетным");
+                }
+                _third = value;
+            }
+        }
+        public bool CompareTwo(Triad triad1, Triad triad2) => triad1.First == triad2.First && triad1.Second == triad2.Second && triad1.Third == triad2.Third;
+        public bool CompareThree(Triad triad1, Triad triad2, Triad triad3) => triad1.First == triad2.First && triad3.First == triad2.First 
+            && triad1.Second == triad2.Second && triad3.Second == triad2.Second
+            && triad1.Third == triad2.Third && triad3.Third == triad2.Third;
+    }
+}
